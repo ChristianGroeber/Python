6f1e8f1e1560cc62b5c0aa667307f36b79535f18
ChristianGroeber <40316753+ChristianGroeber@users.noreply.github.com>
2018-12-17 20:05:01 +0100

    changed settings.py, trying to get CSS to work

--- Django/AnnesNaehkaestchen/AnnesNaehkaestchen/settings.py
+++ Django/AnnesNaehkaestchen/AnnesNaehkaestchen/settings.py
@@ -127,6 +127,4 @@ STATIC_URL = '/static/'
 
 #SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    #os.path.join(SITE_ROOT, 'static/')
-)
+
