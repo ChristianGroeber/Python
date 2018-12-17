949342a8c76149ff580a0d7243e4cf220005e3c6
ChristianGroeber <40316753+ChristianGroeber@users.noreply.github.com>
2018-12-17 19:45:57 +0100

    changed settings.py, trying to get CSS to work

--- Django/AnnesNaehkaestchen/AnnesNaehkaestchen/settings.py
+++ Django/AnnesNaehkaestchen/AnnesNaehkaestchen/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from django.conf import settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django.conf.global_settings import STATICFILES_DIRS
@@ -122,6 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATICFILES_DIRS = '/projekt/static'
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+    os.path.join(SITE_ROOT, 'static/')
+)
