7e50531429d6f1497855814c0cc5c1a33f2b583e
ChristianGroeber <40316753+ChristianGroeber@users.noreply.github.com>
2018-12-17 19:51:40 +0100

    changed settings.py, trying to get CSS to work

--- Django/AnnesNaehkaestchen/AnnesNaehkaestchen/urls.py
+++ Django/AnnesNaehkaestchen/AnnesNaehkaestchen/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf import settings
+from AnnesNaehkaestchen import settings
 
 urlpatterns = [
     path('', include('projekt.urls')),
