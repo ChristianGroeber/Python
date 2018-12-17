53dfa471b9e14cb74807f88eac6c711bc2e32cac
ChristianGroeber <40316753+ChristianGroeber@users.noreply.github.com>
2018-12-17 17:42:14 +0100

    changed settings.py

--- Django/AnnesNaehkaestchen/AnnesNaehkaestchen/settings.py
+++ Django/AnnesNaehkaestchen/AnnesNaehkaestchen/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.conf.global_settings import STATICFILES_DIRS
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -121,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = '/projekt/static'
